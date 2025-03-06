4446092646facd15bbcf4cd50207cd2cef85e038
Your Name <essam.mohamed.dev@gmail.com>
2025-03-07 01:40:55 +0200

    update

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,10 @@
-import { HiOutlineSparkles } from "react-icons/hi";
+import Image from "@node_modules/next/image";
+
 export default function Header() {
   return (
     <header className="text-center space-y-4">
       <div className="inline-flex p-3 bg-blue-100 rounded-full animate-spin hover:animate-none transition-all">
-        <HiOutlineSparkles className="w-8 h-8 text-primary" />
+        <Image src="/logo.png" width={40} height={40} alt="logo" />
       </div>
 
       <h1 className="text-4xl font-bold text-secondary transition-transform hover:scale-105">
